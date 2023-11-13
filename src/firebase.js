05d94d7d1dc6549fecfdd6e1b04d492639a72471
vk <justvk2013@gmail.com>
2023-11-12 22:33:40 -0700

    corr

--- src/firebase.js
+++ src/firebase.js
@@ -22,6 +22,6 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
-const analytics = getAnalytics(app);
+// const analytics = getAnalytics(app);
 
 export const db = getFirestore(app);
